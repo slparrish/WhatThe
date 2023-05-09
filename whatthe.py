889cd387af2d3432162825b9314c652bff83bc75
Scott <sparrish25@ivytech.edu>
2023-05-09 18:46:40 -0400

    added images

--- whatthe.py
+++ whatthe.py
@@ -5,6 +5,9 @@
 # GitHub: https://github.com/slparrish/WhatThe.git
 
 from breezypythongui import EasyFrame
+from tkinter import PhotoImage
+# from tkinter.font import Font
+# import sys
 import platform
 import socket
 import shutil
@@ -14,23 +17,32 @@ class WhatThe(EasyFrame):
     
     def __init__(self,
                 title="WhatThe",
-                width=600,
-                height=400,
+                width=800,
+                height=600,
                 background="white",
                 resizable=True): 
                
         super().__init__(title, width, height, background, resizable)
 
+        self.imgfile = f'{platform.system().lower()}.gif'
+
         # Potentially panelize groups
         # General Group
         self.genLabel = self.addLabel('General Information:', row=0, column=0, sticky="SW")
         self.genOutputArea = self.addTextArea("", row=1, column=0, width=5, height=5)
+        imageLabel = self.addLabel("", row=1, column=1, sticky="NSEW") 
+        self.image = PhotoImage(file=self.imgfile)
+        imageLabel["image"] = self.image
         # Networking group
         self.netLabel = self.addLabel('Networking:', row=2, column=0, sticky="SW")
         self.netOutputArea = self.addTextArea("", row=3, column=0, width=5, height=5)
 
         self.addButton('Refresh', row=3, column=1)  # need function to refresh info in HostInfo
         self.addButton('Exit', row=4, column=1, command=exit)
+    
+    def setImage(self, imgfile):
+        self.imgfile = imgfile
+
 
 # HostInfo() class to gather info about our machine    
 # To do: potentially move some methods into constructor, add more info
@@ -72,12 +84,16 @@ class HostInfo():
 def main() -> None:
     root = WhatThe()
     myHost = HostInfo()
+    root.setImage("mac.gif")
     genString = f'Hostname: {myHost.getHostname()}\n\
 System: {myHost.system()}\n\
 Version: {myHost.version()}\n\
 Disk Usage: Total:{myHost.getDisk("/")[0]/1000000000:.1f} GB \n\
             Used: {myHost.getDisk("/")[1]/1000000000:.1f} GB\n\
             Free: {myHost.getDisk("/")[2]/1000000000:.1f} GB'
+    # root.image = PhotoImage(file="mac.gif")
+    # root.image = PhotoImage(file="mac.gif")
+    # imageLabel["Image"] = root.image
     root.genOutputArea.setText(genString)
     netString = f'IPv4 Address: {myHost.getIP2()}'
     root.netOutputArea.setText(netString)
